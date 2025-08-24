a37cab037469ca41f56b4c6779bf7aebe27982e3
renzito0637d <renzito0637d@gmail.com>
2025-08-23 22:49:26 -0500

    Creación de archivos de spring para el proyecto

---
+++ src/main/java/com/heramientadesarrollo/HerramientaDesarrolloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heramientadesarrollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerramientaDesarrolloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerramientaDesarrolloBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heramientadesarrollo/HerramientaDesarrolloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heramientadesarrollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerramientaDesarrolloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
