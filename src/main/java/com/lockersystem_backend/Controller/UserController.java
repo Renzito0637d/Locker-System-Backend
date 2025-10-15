a45f583692ee31f0bb30d2a583e32631bf863615
JuanHurtado <U22314908@utp.edu.pe>
2025-10-14 23:57:47 -0500

    Implementacion del crud para User

---
+++ src/main/java/com/lockersystem_backend/Controller/UserController.java
@@ -0,0 +1,58 @@
+package com.lockersystem_backend.Controller;
+
+import com.lockersystem_backend.Entity.User;
+import com.lockersystem_backend.Service.Interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.findById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        Optional<User> userOptional = userService.findById(id);
+        if (!userOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        User user = userOptional.get();
+    user.setUserName(userDetails.getUserName());
+    user.setPasswordHash(userDetails.getPasswordHash());
+    user.setEmail(userDetails.getEmail());
+    user.setRoles(userDetails.getRoles());
+    // Agrega aquí otros campos que quieras actualizar
+    User updatedUser = userService.save(user);
+    return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        if (!userService.findById(id).isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
